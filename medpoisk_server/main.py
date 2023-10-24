e898a141d27e05d3a53694b99127811cf44cd2d6
honanaipr <honanaipr@github.com>
2023-10-25 02:24:14 +0300

    separate v0 version

--- medpoisk_server/main.py
+++ medpoisk_server/main.py
@@ -5,10 +5,13 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.include_router(items.router, prefix="/api/v0")
-app.include_router(rooms.router, prefix="/api/v0")
-app.include_router(doctors.router, prefix="/api/v0")
-app.include_router(places.router, prefix="/api/v0")
+v1 = FastAPI()
+app.mount("/api/v0", v1)
+
+v1.include_router(items.router)
+v1.include_router(rooms.router)
+v1.include_router(doctors.router)
+v1.include_router(places.router)
 
 app.mount(
     "/", StaticFiles(directory="../medpoisk-client/dist", html=True), name="static"
