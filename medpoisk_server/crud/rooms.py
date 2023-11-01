b296531e5650b60be28d56da61bab08f2d72dc27
honanaipr <honanaipr@github.com>
2023-11-01 19:37:53 +0300

    add return model

--- medpoisk_server/crud/rooms.py
+++ medpoisk_server/crud/rooms.py
@@ -1,12 +1,11 @@
 from sqlalchemy.orm import Session
-from uuid import UUID
 from .. import models, schemas
 
 
 def get_rooms(db: Session, skip: int = 0, limit: int = 100) -> list[models.Room]:
     return db.query(models.Room).offset(skip).limit(limit).all()
 
-def get_room(db: Session, room_number: int):
+def get_room(db: Session, room_number: int) -> models.Room:
         return db.query(models.Room).where(models.Room.number == room_number).one()
 
 def create_room(db: Session, room: schemas.RoomCreate) -> models.Room:
