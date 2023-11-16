12bdb2fb5f323815b0ada37944fdbdb8e20ed9f7
honanaipr <honanaipr@github.com>
2023-11-16 09:54:36 +0300

    rename images to pictures

--- medpoisk_server/routers/pictures.py
+++ medpoisk_server/routers/pictures.py
@@ -9,8 +9,8 @@ from typing import Annotated
 import os
 
 router = APIRouter(
-    prefix="/images",
-    tags=["images"],
+    prefix="/pictures",
+    tags=["pictures"],
 )
 
 @router.post("/{id}")
@@ -20,9 +20,9 @@ async def create_file(id: UUID, file: UploadFile, db: Session = Depends(get_db))
         with open(file_path, 'wb') as f:
             content = await file.read()
             f.write(content)
-        crud.set_product_picture_url(db, id, f"http://медпоиск.рф/pictures/{id}.{file.filename.split('.')[-1]}")
+        crud.set_product_picture_url(db, id, f"/pictures/{id}.{file.filename.split('.')[-1]}")
     except Exception as e:
         if os.path.exists(file_path):
             os.remove(file_path)
     db.commit()
-    return f"http://медпоиск.рф/pictures/{id}.{file.filename.split('.')[-1]}"
+    return f"/pictures/{id}.{file.filename.split('.')[-1]}"
