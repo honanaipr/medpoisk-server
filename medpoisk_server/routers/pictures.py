6a92db54511aab300df2e06f7e5fd1b9f5c1aeda
honanaipr <honanaipr@github.com>
2023-12-05 00:56:16 +0300

    pictures path

--- medpoisk_server/routers/pictures.py
+++ medpoisk_server/routers/pictures.py
@@ -20,7 +20,7 @@ async def create_file(id: UUID, file: UploadFile, db: Session = Depends(get_db))
         with open(file_path, 'wb') as f:
             content = await file.read()
             f.write(content)
-        crud.set_product_picture_url(db, id, f"{config.pictures_dir}/{id}.{file.filename.split('.')[-1]}")
+        crud.set_product_picture_url(db, id, f"/pictures/{id}.{file.filename.split('.')[-1]}")
     except Exception as e:
         if os.path.exists(file_path):
             os.remove(file_path)
