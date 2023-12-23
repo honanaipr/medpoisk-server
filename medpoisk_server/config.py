41be1f8d84e4c89f57913e91fffd1b431b6bcdce
honanaipr <honanaipr@github.com>
2023-12-23 16:21:16 +0300

    change port to 8000 due to permissins

--- medpoisk_server/config.py
+++ medpoisk_server/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     pictures_dir: str = Field("./pictures")
     sqlalchemy_db_url: str = Field("postgresql://postgres:password@localhost/medpoisk")
     static_path: str = Field("static")
-    port: int = Field(80)
+    port: int = Field(8000)
     jwt_settings: JWTSettings = JWTSettings()
 
 
