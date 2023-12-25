81b5ac3d3391c80eff38fcbba0e4a9921a92a2cb
honanaipr <honanaipr@github.com>
2023-12-25 07:25:19 +0300

    make representable models

--- medpoisk_server/database.py
+++ medpoisk_server/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import config
+from sqlalchemy_repr import RepresentableBase
 
 engine = create_engine(config.sqlalchemy_db_url, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base = declarative_base(cls=RepresentableBase)
