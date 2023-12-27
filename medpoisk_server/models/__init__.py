ef61b6a038589296808964e2f6453926b3944ef2
honanaipr <honanaipr@github.com>
2023-12-27 04:46:39 +0300

    add __all__ to models __init__

---
+++ medpoisk_server/models/__init__.py
@@ -0,0 +1,23 @@
+from .division import Division
+from .doctor import Doctor
+from .employee import Employee
+from .inventory import Inventory
+from .place import Place
+from .position import Position
+from .privilage import Privilage
+from .product import Product
+from .role import Role
+from .room import Room
+
+__all__ = [
+    "Division",
+    "Doctor",
+    "Employee",
+    "Inventory",
+    "Place",
+    "Position",
+    "Privilage",
+    "Product",
+    "Role",
+    "Room",
+]
