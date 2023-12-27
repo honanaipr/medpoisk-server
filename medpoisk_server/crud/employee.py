c6e7441cdf7d747ad1ec6f8bcbb0abd4b6675ff8
honanaipr <honanaipr@github.com>
2023-12-27 04:55:24 +0300

    add get_roles_by_employee_id

--- medpoisk_server/crud/employee.py
+++ medpoisk_server/crud/employee.py
@@ -1,3 +1,4 @@
+from pydantic.type_adapter import TypeAdapter
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -6,9 +7,15 @@ from .. import models, schemas
 
 def get_employee_by_username(
     username: str, session: Session
-) -> schemas.EmployeePrivate | None:
+) -> schemas.EmployeePrivate:
     stmt = select(models.Employee).where(models.Employee.username == username)
     db_employee = session.scalar(stmt)
-    if not db_employee:
-        return None
     return schemas.EmployeePrivate.model_validate(db_employee)
+
+
+def get_roles_by_employee_id(
+    employee_id: int, session: Session
+) -> list[schemas.RoleInDivision]:
+    stmt = select(models.Privilage).where(models.Privilage.employee_id == employee_id)
+    results = session.scalars(stmt)
+    return TypeAdapter(list[schemas.RoleInDivision]).validate_python(list(results))
