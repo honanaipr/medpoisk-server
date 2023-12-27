48536ef34056734ffe2b4567c3e8a9da6729ac48
honanaipr <honanaipr@github.com>
2023-12-27 04:27:24 +0300

    add check of token data in auth test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from medpoisk_server.main import app
+from medpoisk_server.security import jwt_decode
 
 client = TestClient(app)
 
@@ -8,8 +9,8 @@ client = TestClient(app)
 def get_token() -> str:
     data = data = {
         "grant_type": "",
-        "username": "ivanov92",
-        "password": "ivanov92",
+        "username": "petrovna_rus",
+        "password": "petrovna_rus",
         "scope": "",
         "client_id": "",
         "client_secret": "",
@@ -32,10 +33,16 @@ def test_get_token():
     assert response.json()["access_token"]
     assert response.json()["token_type"] == "bearer"
     assert response.cookies["refreshToken"]
+    assert jwt_decode(response.json()["access_token"]).username
+    assert jwt_decode(response.json()["access_token"]).roles
+    assert jwt_decode(response.json()["access_token"]).roles[0].division
+    assert jwt_decode(response.json()["access_token"]).roles[0].role_name
 
 
 def test_refresh_token():
-    response = client.post("/api/v0/auth/refresh")
+    response = client.post(
+        "/api/v0/auth/refresh", cookies={"Authorization": f"Bearer {get_token()}"}
+    )
     assert response.status_code == 200
     assert response.json()["access_token"]
     assert response.json()["token_type"] == "bearer"
