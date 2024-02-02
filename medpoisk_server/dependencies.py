2ae35d1a2cae64afc499c3defbd604ae8988e9a2
honanaipr <rubtm.r@gmail.com>
2024-02-02 17:10:38 +0300

    add get_auntificated_places dependency

--- medpoisk_server/dependencies.py
+++ medpoisk_server/dependencies.py
@@ -3,10 +3,11 @@ from typing import Annotated
 import jwt
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+from pydantic.type_adapter import TypeAdapter
 from sqlalchemy.orm import Session
 
 from . import http_exceptions, schemas, security
-from .crud import get_employee_by_username
+from .crud import get_employee_by_username, get_places
 from .database import SessionLocal
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
@@ -48,3 +49,13 @@ def get_auntificated_divisions(
     token_data: Annotated[schemas.TokenData, Depends(get_verified_token_data)],
 ) -> list[schemas.DivisionPublick]:
     return [role.division for role in token_data.roles]
+
+
+def get_auntificated_places(
+    divisions: Annotated[
+        list[schemas.DivisionPublick], Depends(get_auntificated_divisions)
+    ],
+    session: Annotated[Session, Depends(get_db)],
+) -> list[schemas.PlacePublick]:
+    auntificated_plcaces = get_places(session, [division.id for division in divisions])
+    return TypeAdapter(list[schemas.PlacePublick]).validate_python(auntificated_plcaces)
