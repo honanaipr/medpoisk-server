7094d33cff2a769eed09c9fc6b1171c2ba0482b8
honanaipr <rubtm.r@gmail.com>
2024-02-02 17:15:46 +0300

    patch staff router

--- medpoisk_server/crud/profile_crud.py
+++ medpoisk_server/crud/profile_crud.py
@@ -12,13 +12,16 @@ def flatten_divisions(S):
 
 
 def get_staff(
-    db: Session, division_ids: list[int]
+    db: Session, division_ids: list[int], role_name: schemas.Role | None = None
 ) -> list[schemas.EmployeePublicDetailed]:
-    db_employee = db.scalars(
+    stmt = (
         select(models.Employee)
         .join(models.Employee.privilages)
         .where(models.Privilage.division_id.in_(division_ids))
     )
+    if role_name is not None:
+        stmt = stmt.where(models.Privilage.role_name == role_name)
+    db_employee = db.scalars(stmt)
     return TypeAdapter(list[schemas.EmployeePublicDetailed]).validate_python(
         db_employee, from_attributes=True
     )
